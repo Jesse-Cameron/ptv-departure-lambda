0dfc262be8e2faa06b0e95b41d9dd96d6ca5e916
Jesse-Cameron <jessec.cameron@gmail.com>
2023-04-11 15:09:45 +1000

    feat: parse ptv responses into minutes until next departure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use lambda_runtime::LambdaEvent;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
@@ -69,13 +70,23 @@ async fn handler(_e: LambdaEvent<Request>, settings: Settings) -> Response {
         body: format!("did not successfully complete request. {}", err.to_string()),
     })?;
 
-    let (_res_body_1, _res_body_2) = try_join!(
+    let (res_to_city, res_from_city) = try_join!(
         res_to_city.json::<ptv::ViewDeparturesResponse>(),
         res_from_city.json::<ptv::ViewDeparturesResponse>()
     )
     .map_err(|err| FailureResponse {
         body: format!("could not read json response. {}", err.to_string()),
     })?;
+
+    let _from_city_minutes =
+        get_departure_minutes_from_response(res_from_city).map_err(|err| FailureResponse {
+            body: format!("could not get departure minutes. {}", err.to_string()),
+        })?;
+
+    let _to_city_minutes =
+        get_departure_minutes_from_response(res_to_city).map_err(|err| FailureResponse {
+            body: format!("could not get departure minutes. {}", err.to_string()),
+        })?;
 
     Ok(SuccessResponse {})
 }
@@ -111,9 +122,44 @@ fn create_request(
     Ok(res)
 }
 
+type MaybeDepartureMins = Result<(i64, Option<i64>), Box<dyn std::error::Error>>;
+
+fn get_departure_minutes_from_response(
+    response: ptv::ViewDeparturesResponse,
+) -> MaybeDepartureMins {
+    let first_departure = response.departures.get(0).ok_or("no departures found")?;
+    let first_time = get_minutes_from_departure(first_departure.clone())?;
+
+    let second_time = response
+        .departures
+        .get(1)
+        // note: we'll swallow an error here, in favour of returning the first departure time
+        // in the future we might want to log/handle this better?
+        .map(|d| get_minutes_from_departure(d.clone()).ok())
+        .flatten();
+
+    Ok((first_time, second_time))
+}
+
+fn get_minutes_from_departure(
+    departure: ptv::Departure,
+) -> Result<i64, Box<dyn std::error::Error>> {
+    let arrival_ts = departure
+        .estimated_departure_utc
+        .or(departure.scheduled_departure_utc)
+        .ok_or("could not find timestamps from departure")?;
+
+    let utc = DateTime::parse_from_rfc3339(&arrival_ts)?;
+    let now = Utc::now();
+    let duration = utc.signed_duration_since(now).num_minutes();
+    Ok(duration)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::ptv::Departure;
+    use chrono::Duration;
 
     #[test]
     fn test_create_successful_request() {
@@ -122,5 +168,124 @@ mod tests {
         let uri = "http://example.com";
         let res = create_request(&http_client, api_key, 32, 1, uri.to_string()).unwrap();
         assert_eq!(res.url().as_str(), "http://example.com/v3/departures/route_type/0/stop/1170?platform=1&max_results=2&include_cancelled=false&devid=32&signature=e4526cb4ce2791d438844077d8a1869ce8fe83ca")
+    }
+
+    #[test]
+    fn test_get_minutes_from_response_calc() {
+        let tests = vec![
+            (190, 3),  // three minutes away
+            (179, 2),  // under three minutes away
+            (0, 0),    // zero
+            (-5, 0),   // five seconds ago
+            (-60, -1), // one minute ago
+        ];
+
+        for (future_secs, expected_mins) in tests {
+            let test_time = Utc::now() + Duration::seconds(future_secs);
+            let test_departure = Departure {
+                scheduled_departure_utc: Some(
+                    test_time.to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+                ),
+                ..Default::default()
+            };
+            let result =
+                get_minutes_from_departure(test_departure).expect("failed to find departure times");
+            assert_eq!(result, expected_mins)
+        }
+    }
+
+    #[test]
+    fn test_get_minutes_from_response_prioritise_estimated() {
+        let test_time_1 = Utc::now() + Duration::seconds(60);
+        let test_time_2 = Utc::now() + Duration::seconds(130);
+        let test_departure = Departure {
+            scheduled_departure_utc: Some(
+                test_time_1.to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+            ),
+            estimated_departure_utc: Some(
+                test_time_2.to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+            ),
+            ..Default::default()
+        };
+
+        let result =
+            get_minutes_from_departure(test_departure).expect("failed to find departure times");
+        assert_eq!(result, 2)
+    }
+
+    #[test]
+    fn test_get_minutes_for_responses_empty() {
+        let test_departure = Departure {
+            estimated_departure_utc: None,
+            scheduled_departure_utc: None,
+            ..Default::default()
+        };
+
+        let result = get_minutes_from_departure(test_departure);
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "could not find timestamps from departure"
+        );
+    }
+
+    #[test]
+    fn test_get_departure_minutes_from_response() {
+        let test_cases: Vec<(ptv::ViewDeparturesResponse, MaybeDepartureMins)> = vec![
+            (
+                ptv::ViewDeparturesResponse { departures: vec![] },
+                Err("no departures found".into()),
+            ),
+            (
+                ptv::ViewDeparturesResponse {
+                    departures: vec![
+                        departure_in(Duration::minutes(9)),
+                        ptv::Departure {
+                            ..Default::default()
+                        },
+                    ],
+                },
+                Ok((8, None)),
+            ),
+            (
+                ptv::ViewDeparturesResponse {
+                    departures: vec![
+                        departure_in(Duration::minutes(3)),
+                        departure_in(Duration::minutes(8)),
+                    ],
+                },
+                Ok((2, Some(7))),
+            ),
+            (
+                ptv::ViewDeparturesResponse {
+                    departures: vec![
+                        ptv::Departure {
+                            scheduled_departure_utc: Some("invalid".to_string()),
+                            ..Default::default()
+                        },
+                        departure_in(Duration::minutes(1)),
+                    ],
+                },
+                Err("input contains invalid characters".into()),
+            ),
+        ];
+
+        for (response, expected) in test_cases {
+            let actual: MaybeDepartureMins = get_departure_minutes_from_response(response.clone());
+            match expected {
+                Ok(minutes) => assert_eq!(minutes, actual.unwrap()),
+                Err(e) => assert_eq!(e.to_string(), actual.unwrap_err().to_string()),
+            }
+        }
+    }
+
+    fn departure_in(time_from_now: Duration) -> ptv::Departure {
+        let departure_time = Utc::now() + time_from_now;
+        ptv::Departure {
+            scheduled_departure_utc: Some(
+                departure_time.to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+            ),
+            ..Default::default()
+        }
     }
 }
--- src/ptv.rs
+++ src/ptv.rs
@@ -1,11 +1,11 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, Default)]
 pub struct ViewDeparturesResponse {
     pub departures: Vec<Departure>,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone, Default)]
 pub struct Departure {
     pub at_platform: bool,
     pub scheduled_departure_utc: Option<String>,
