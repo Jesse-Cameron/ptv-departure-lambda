d36bb487091c1fa7782d640d77ff7bf32b08f577
Jesse-Cameron <jessec.cameron@gmail.com>
2023-04-13 18:50:40 +1000

    fix: incorrect query in ptv request

--- src/main.rs
+++ src/main.rs
@@ -96,7 +96,7 @@ async fn handler(_e: LambdaEvent<Request>, settings: Settings) -> Response {
         });
     }
 
-    let (res_to_city, res_from_city) = try_join!(
+    let (json_to_city, json_from_city) = try_join!(
         res_to_city.json::<ptv::ViewDeparturesResponse>(),
         res_from_city.json::<ptv::ViewDeparturesResponse>()
     )
@@ -105,12 +105,12 @@ async fn handler(_e: LambdaEvent<Request>, settings: Settings) -> Response {
     })?;
 
     let from_city_departures =
-        get_departure_minutes_from_response(res_from_city).map_err(|err| FailureResponse {
+        get_departure_minutes_from_response(json_from_city).map_err(|err| FailureResponse {
             body: format!("could not get departure minutes. {}", err.to_string()),
         })?;
 
     let to_city_departures =
-        get_departure_minutes_from_response(res_to_city).map_err(|err| FailureResponse {
+        get_departure_minutes_from_response(json_to_city).map_err(|err| FailureResponse {
             body: format!("could not get departure minutes. {}", err.to_string()),
         })?;
 
@@ -132,7 +132,7 @@ fn create_request(
     let max_departures: u8 = 2; // we only want the next two departures
 
     let path = format!(
-        "/v3/departures/route_type/{}/stop/{}?platform={}&max_results={}&include_cancelled=false&devid={}",
+        "/v3/departures/route_type/{}/stop/{}?platform_numbers={}&max_results={}&include_cancelled=false&devid={}",
         route_type, stop_id, platform_id, max_departures, developer_id
     );
 
@@ -205,7 +205,7 @@ mod tests {
         let api_key: &[u8] = b"9c132d31-6a30-4cac-8d8b-8a1970834799"; // fake key
         let uri = "http://example.com";
         let res = create_request(&http_client, api_key, 32, 1, uri.to_string()).unwrap();
-        assert_eq!(res.url().as_str(), "http://example.com/v3/departures/route_type/0/stop/1170?platform=1&max_results=2&include_cancelled=false&devid=32&signature=e4526cb4ce2791d438844077d8a1869ce8fe83ca")
+        assert_eq!(res.url().as_str(), "http://example.com/v3/departures/route_type/0/stop/1170?platform_numbers=1&max_results=2&include_cancelled=false&devid=32&signature=234004d132ed696e31cbb23f703743df0f2d7ae3")
     }
 
     #[test]
@@ -342,7 +342,8 @@ mod tests {
         let _m = server
             .mock("GET", "/v3/departures/route_type/0/stop/1170")
             .match_query(mockito::Matcher::Regex(
-                "platform=1&max_results=2&include_cancelled=false&devid=1&signature=*".into(),
+                "platform_numbers=1&max_results=2&include_cancelled=false&devid=1&signature=*"
+                    .into(),
             ))
             .with_body(example_response_body_1)
             .create_async()
@@ -351,7 +352,8 @@ mod tests {
         let _m = server
             .mock("GET", "/v3/departures/route_type/0/stop/1170")
             .match_query(mockito::Matcher::Regex(
-                "platform=2&max_results=2&include_cancelled=false&devid=1&signature=*".into(),
+                "platform_numbers=2&max_results=2&include_cancelled=false&devid=1&signature=*"
+                    .into(),
             ))
             .with_body(example_response_body_2)
             .create_async()
