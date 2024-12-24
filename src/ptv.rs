0c0e3c6d419554bb601e41f5ba2e67708cf79a53
Jesse-Cameron <jessec.cameron@gmail.com>
2024-12-24 22:23:25 +1100

    chore(lint): remove unused field

--- src/ptv.rs
+++ src/ptv.rs
@@ -7,7 +7,6 @@ pub struct ViewDeparturesResponse {
 
 #[derive(Debug, Deserialize, Clone, Default)]
 pub struct Departure {
-    pub at_platform: bool,
     pub scheduled_departure_utc: Option<String>,
     pub estimated_departure_utc: Option<String>,
 }
